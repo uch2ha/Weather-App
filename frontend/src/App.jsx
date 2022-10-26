81eee4007c4b2b44759f88570da8603b7e46f06c
uch2ha <sinyavskiy.d@gmail.com>
2022-10-26 17:13:59 +0300

    new frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+// const baseURL = process.env.ENDPOINT || 'http://localhost:9000/api';
+
+// const getWeatherFromApi = async (city) => {
+//   try {
+//     const response = await fetch(`${baseURL}/weatherbycity?city=${city}`);
+//     return response.json();
+//   } catch (error) {
+//     console.error(error);
+//   }
+
+//   return {};
+// };
+import React from 'react';
+
+const App = () => {
+  return <div>App init</div>;
+};
+
+export default App;
