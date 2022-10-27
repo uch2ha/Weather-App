22ec7ffaba0db27122d8e19d8a1f88bf40cb431f
uch2ha <sinyavskiy.d@gmail.com>
2022-10-27 11:19:54 +0300

    backend tests are done

--- backend/test/test_1.js
+++ backend/test/test_1.js
@@ -1,10 +1,10 @@
 import kill from 'kill-port';
 import chai from 'chai';
-import { should, expect } from 'chai';
 import chaiHttp from 'chai-http';
+const port = process.env.PORT || 9000;
 
-//! Unfortunately tests are not working without starting the backend by "npm start"
-//! I tried my best
+//! start server
+import app from '../src/index.js';
 
 //Assertion Style
 chai.should();
@@ -18,13 +18,17 @@ describe('Test', () => {
     it('It should GET weather by city', (done) => {
       const city = 'London';
       chai
-        .request('http://localhost:9000')
+        .request(`http://localhost:${port}`)
         .get('/api/weatherbycity')
         .query({ city: city })
         .end((err, res) => {
           res.should.have.status(200);
           res.should.have.property('body');
           res.body.should.have.property('name').eq(city);
+          res.body.should.have.property('weather');
+          res.body.weather[0].should.have.property('id');
+          res.body.weather[0].should.have.property('main');
+          res.body.weather[0].should.have.property('description');
           done();
         });
     });
@@ -37,7 +41,7 @@ describe('Test', () => {
     it('It should GET forecast by city', (done) => {
       const city = 'London';
       chai
-        .request('http://localhost:9000')
+        .request(`http://localhost:${port}`)
         .get('/api/forecast')
         .query({ city: city })
         .end((err, res) => {
@@ -49,6 +53,59 @@ describe('Test', () => {
           res.body.weather.should.have.property('main');
           res.body.weather.should.have.property('description');
           done();
+        });
+    });
+  });
+
+  /* 
+      Get weather data by coordinates
+    */
+  describe('GET /api/weatherbycoordinates', () => {
+    it('It should GET weather by coordinates', (done) => {
+      const lon = 24.9414;
+      const lat = 60.1718;
+      const city = 'Helsinki';
+      chai
+        .request(`http://localhost:${port}`)
+        .get('/api/weatherbycoordinates')
+        .query({ lon: lon, lat: lat })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.have.property('body');
+          res.body.should.have.property('name').eq(city);
+          res.body.should.have.property('weather');
+          res.body.should.have.property('coord');
+          res.body.weather[0].should.have.property('id');
+          res.body.weather[0].should.have.property('main');
+          res.body.weather[0].should.have.property('description');
+          res.body.coord.should.have.property('lon').eq(lon);
+          res.body.coord.should.have.property('lat').eq(lat);
+          done();
+        });
+    });
+  });
+  /* 
+      Get forecast by coordinates
+    */
+  describe('GET /api/forecastbycoordinates', () => {
+    it('It should GET forecast by coordinates', (done) => {
+      const lon = 24.9414;
+      const lat = 60.1718;
+      chai
+        .request(`http://localhost:${port}`)
+        .get('/api/forecastbycoordinates')
+        .query({ lon: lon, lat: lat })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.have.property('body');
+          res.body.should.have.property('weather');
+          res.body.should.have.property('time');
+          res.body.weather.should.have.property('id');
+          res.body.weather.should.have.property('main');
+          res.body.weather.should.have.property('description');
+          done();
+          //! close server
+          kill(port);
         });
     });
   });
