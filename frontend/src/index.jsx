c0debf0d1b24c438beca7b4d7b23bde5df26b3b9
uch2ha <sinyavskiy.d@gmail.com>
2022-10-28 18:34:35 +0300

    fix scalability problem

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './components/mainPage/MainPage';
+import './test.css';
 
 ReactDOM.render(<MainPage />, document.getElementById('app'));
